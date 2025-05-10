1c8c1590ca080ee59c7adc6b0100a19ab4809c91
James Tuttle <james.j.tuttle@gmail.com>
2025-05-10 10:31:47 -0700

    finsihed terraform main.

--- terraform-js/main.tf
+++ terraform-js/main.tf
@@ -7,7 +7,7 @@ resource "aws_s3_bucket" "nextjs_bucket" {
   bucket = "nextjs-portfolio-bucket-tuttle"
 }
 
-# Ownership Control
+# Ownership Control of ALL Objects in bucket
 resource "aws_s3_bucket_ownership_controls" "nextjs_bucket_ownership_control" {
   bucket = aws_s3_bucket.nextjs_bucket.id
 
@@ -57,3 +57,52 @@ resource "aws_s3_bucket_policy" "nextjs_bucket_policy" {
 }
 
 
+# Origin Access Identity
+resource "aws_cloudfront_origin_access_identity" "origin_access_identity" {
+    comment = "OAI for Next.JS Portfolio Site."
+}
+
+# Cloudfront distribution
+resource "aws_cloudfront_distribution" "nextjs_distribution" {
+    origin {
+        domain_name = aws_s3_bucket.nextjs_bucket.bucket_regional_domain_name,
+        origin_id = "S3-${aws_s3_bucket.nextjs_bucket.id}"
+
+        s3_origin_config {
+            origin_access_identity = aws_cloudfront_origin_access_identity.origin_access_identity.cloudfront_access_identity_path
+        }
+    }
+    
+    enabled = true
+    is_ipv6_enabled = true
+    comment = "Next.js portfolio site"
+    default_root_object = "index.html"
+
+    default_cache_behavior {
+        allowed_methods = ["GET", "HEAD", "OPTIONS"]
+        cached_methods = ["GET", "HEAD"]
+        target_origin_id = "S3-${aws_s3_bucket.nextjs_bucket.id}"
+        viewer_protocol_policy = "redirect-to-https"
+        min_ttl = 0
+        default_ttl = 3600
+        max_ttl = 86400
+
+        forwarded_values {
+        query_string = false
+        cookies {
+            forward = "none"
+        }
+    }
+
+}
+restrictions {
+    geo_restriction {
+        restriction_type = "none"
+    }
+}
+viewer_certificate {
+    cloudfront_default_certificate = true
+}
+
+
+}
